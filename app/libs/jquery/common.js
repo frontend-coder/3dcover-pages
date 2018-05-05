b61b6d5e5b081be73fa09d9941bcf4eef32231d6
DESKTOP-DD2BR4M\Front-End Developer <frontendercode@gmail.com>
2018-05-05 12:12:30 +0300

    v.0.1
    Стартовый шаблон для создания страницы  с 3D Cover

---
+++ app/libs/jquery/common.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+	$("body").niceScroll({
+horizrailenabled:false
+});
+// вверхнее красиво-вращающееся меню
+// 1 и 2 строка это анимация крестика
+//3 строка - слайдер вниз меню
+$(".toggle-mnu").click(function() {
+$(this).toggleClass("on");
+$(".hidden-mnu").slideToggle();
+return false;
+});
+$('body, .top-menu ul li a').click(function () {
+$('.hidden-mnu').hide("slow");
+});
+
+
+// всплывающие окна обратной связи позвонить мне
+$("a[href='#call-back']").magnificPopup ({
+  mainClass:'mfp-fade',
+  removalDelay:400,
+  type:'inline',
+});
+
+
+/*чтобы в формах был индивидуальный заголовок */
+$("a[href='#call-back']").click(function(){
+  var dataForm = $(this).data("form");
+  var dataText = $(this).data("text");
+  $(".forms-call h4").text(dataText);
+  $(".forms-call [name=admin-data]").val(dataForm);
+});
+
+
+
+// pagination on lending pages
+$(".top-menu ul li a, .hidden-mnu ul li a").mPageScroll2id({
+layout                 : "auto",
+offset                 : ".top-line",
+scrollEasing           : "linear",
+highlightByNextTarget  : true,
+keepHighlightUntilNext : true,
+autoScrollSpeed        : true,
+scrollSpeed            : 1000
+});
+
+// одинаковой высоты разные по длине ашки
+$(".bl").equalHeights();
+$(".dvd_height").equalHeights();
+
+
+$("form").submit(function() { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function() {
+      $(".forms-calldecor .success").addClass("active");
+      setTimeout(function() {
+        // Done Functions
+        $(".forms-calldecor .success").removeClass("active");
+        th.trigger("reset");
+        $.magnificPopup.close();
+      }, 3000);
+    });
+    return false;
+  });
+//ниже вставлять js код
+
+
+
+
+});
